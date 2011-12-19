c60f610ca9faf110aea6e9febcc376c3b76b19d5
Hermann Loose <hermannloose@gmail.com>
2011-12-19 18:52:07 +0100

    Add empty Rails project generated by 'rails new'.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Escalator::Application.config.secret_token = '888a4a83754a14e07fe4cc073b3eb7ae5910fd34d5e811965b9d125c5b2568a38414baf09f6ba8b9d3e41d5b701d3f133d2c220ac4829f6d776b2550023ea855'

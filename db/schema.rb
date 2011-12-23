2fc64fb93ad6cf4ba642057b69296e96b4e74691
Hermann Loose <hermannloose@gmail.com>
2011-12-23 11:18:50 +0100

    Run migration for escalation policies.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111222195859) do
+ActiveRecord::Schema.define(:version => 20111223101804) do
 
   create_table "delayed_jobs", :force => true do |t|
     t.integer  "priority",   :default => 0
@@ -27,6 +27,12 @@ ActiveRecord::Schema.define(:version => 20111222195859) do
   end
 
   add_index "delayed_jobs", ["priority", "run_at"], :name => "delayed_jobs_priority"
+
+  create_table "escalation_policies", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "issues", :force => true do |t|
     t.string   "title"

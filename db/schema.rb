54a74e0a7bfd65e359e84302fa6ef05e69d99a8f
Hermann Loose <hermannloose@gmail.com>
2011-12-23 11:12:21 +0100

    Run migration for issues.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111222182005) do
+ActiveRecord::Schema.define(:version => 20111222195859) do
 
   create_table "delayed_jobs", :force => true do |t|
     t.integer  "priority",   :default => 0
@@ -27,5 +27,14 @@ ActiveRecord::Schema.define(:version => 20111222182005) do
   end
 
   add_index "delayed_jobs", ["priority", "run_at"], :name => "delayed_jobs_priority"
+
+  create_table "issues", :force => true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "posted_at"
+    t.string   "status"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end

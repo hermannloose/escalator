4c09ba614bba527ea9e51bfd36e28988680c9f71
Hermann Loose <hermannloose@gmail.com>
2012-01-08 11:18:23 +0100

    Validate presence of name for rotations.

--- test/unit/rotation_test.rb
+++ test/unit/rotation_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class RotationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should validate presence of name" do
+    rotation = Rotation.new
+    assert !rotation.save
+  end
 end

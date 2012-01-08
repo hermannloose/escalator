5422ca8a59d45b06898cdf004cf739a5df6dfe64
Hermann Loose <hermannloose@gmail.com>
2012-01-08 11:18:42 +0100

    Validate presence of name for escalation policies.

--- app/models/escalation_policy.rb
+++ app/models/escalation_policy.rb
@@ -1,2 +1,3 @@
 class EscalationPolicy < ActiveRecord::Base
+  validates :name, :presence => true
 end
--- test/unit/escalation_policy_test.rb
+++ test/unit/escalation_policy_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class EscalationPolicyTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should validate presence of name" do
+    policy = EscalationPolicy.new
+    assert !policy.save
+  end
 end

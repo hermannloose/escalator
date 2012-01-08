da61bbe1928e272240b3b5e0469ec456b7d43bb7
Hermann Loose <hermannloose@gmail.com>
2012-01-08 12:23:04 +0100

    Validate presence of rotation and user in rotation memberships.
    * Use fixtures for the unit test, to make assertions more concise.

--- app/models/rotation_membership.rb
+++ app/models/rotation_membership.rb
@@ -1,4 +1,7 @@
 class RotationMembership < ActiveRecord::Base
   belongs_to :rotation
   belongs_to :user
+
+  validates :rotation_id, :presence => true
+  validates :user_id, :presence => true
 end
--- test/unit/rotation_membership_test.rb
+++ test/unit/rotation_membership_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class RotationMembershipTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @membership = rotation_memberships(:valid)
+  end
+
+  test "should save valid rotation membership" do
+    assert @membership.save
+  end
+
+  test "should validate presence of rotation" do
+    @membership.rotation = nil
+    assert !@membership.save
+  end
+
+  test "should validate presence of user" do
+    @membership.user = nil
+    assert !@membership.save
+  end
 end

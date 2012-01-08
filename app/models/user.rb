4da5408bf37539ad8343013646be45d018a6ef17
Hermann Loose <hermannloose@gmail.com>
2012-01-08 10:51:21 +0100

    Add name and email presence validation for users.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ActiveRecord::Base
   has_many :contact_details
   has_many :rotation_memberships
   has_many :rotations, :through => :rotation_memberships
+
+  validates :name, :presence => true
+  validates :email, :presence => true
 end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should validate presence of name" do
+    user = User.new
+    user.email = "present"
+    assert !user.save
+  end
+
+  test "should validate presence of email" do
+    user = User.new
+    user.name = "present"
+    assert !user.save
+  end
 end

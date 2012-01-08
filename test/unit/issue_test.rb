804c72b68b0e00592ef3b793e9389aa6c3403f9d
Hermann Loose <hermannloose@gmail.com>
2012-01-08 11:17:51 +0100

    Validate presence of title and status for issues.

--- test/unit/issue_test.rb
+++ test/unit/issue_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class IssueTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should validate presence of title" do
+    issue = Issue.new
+    issue.status = "present"
+    assert !issue.save
+  end
+
+  test "should validate presence of status" do
+    issue = Issue.new
+    issue.title = "present"
+    assert !issue.save
+  end
 end

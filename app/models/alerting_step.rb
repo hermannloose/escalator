c08b86ad8d1bd47f26f0d185553a2d392cfc0e91
Hermann Loose <hermannloose@gmail.com>
2012-01-08 16:24:23 +0100

    Validate presence of rotation and contact details in alerting steps.

--- app/models/alerting_step.rb
+++ app/models/alerting_step.rb
@@ -1,4 +1,7 @@
 class AlertingStep < ActiveRecord::Base
   belongs_to :contact_detail
   belongs_to :rotation_membership
+
+  validates :contact_detail_id, :presence => true
+  validates :rotation_membership_id, :presence => true
 end

78ac34f38c887363ee19d7098a5481ed1bf90745
Hermann Loose <hermannloose@gmail.com>
2012-01-08 16:15:36 +0100

    Add alerting steps to rotation memberships.

--- app/models/alerting_step.rb
+++ app/models/alerting_step.rb
@@ -1,3 +1,4 @@
 class AlertingStep < ActiveRecord::Base
   belongs_to :contact_detail
+  belongs_to :rotation_membership
 end
--- app/models/rotation_membership.rb
+++ app/models/rotation_membership.rb
@@ -1,6 +1,7 @@
 class RotationMembership < ActiveRecord::Base
   belongs_to :rotation
   belongs_to :user
+  has_many :alerting_steps
 
   validates :rotation_id, :presence => true
   validates :user_id, :presence => true

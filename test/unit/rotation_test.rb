c4b476795f2dc0f930bdcdfed0ee6c950fef1eb6
Hermann Loose <hermannloose@gmail.com>
2012-01-08 12:22:35 +0100

    Use fixtures for the rotation unit test.

--- test/unit/rotation_test.rb
+++ test/unit/rotation_test.rb
@@ -1,8 +1,16 @@
 require 'test_helper'
 
 class RotationTest < ActiveSupport::TestCase
+  setup do
+    @rotation = rotations(:valid)
+  end
+
+  test "should save valid rotation" do
+    assert @rotation.save
+  end
+
   test "should validate presence of name" do
-    rotation = Rotation.new
-    assert !rotation.save
+    @rotation.name = nil
+    assert !@rotation.save
   end
 end

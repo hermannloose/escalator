6327739dd860e48ffd4ca10527433b664e95e167
Hermann Loose <hermannloose@gmail.com>
2012-01-09 15:08:18 +0100

    Validate presence of key and parent in contact detail properties.

--- app/models/contact_detail_property.rb
+++ app/models/contact_detail_property.rb
@@ -1,3 +1,6 @@
 class ContactDetailProperty < ActiveRecord::Base
   belongs_to :contact_detail
+
+  validates :contact_detail_id, :presence => true
+  validates :key, :presence => true
 end
--- test/unit/contact_detail_property_test.rb
+++ test/unit/contact_detail_property_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class ContactDetailPropertyTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @contact_detail_property = contact_detail_properties(:valid)
+  end
+
+  test "should save valid contact detail property" do
+    assert @contact_detail_property.save
+  end
+
+  test "should validate presence of contact detail" do
+    @contact_detail_property.contact_detail = nil
+    assert !@contact_detail_property.save
+  end
+
+  test "should validate presence of key" do
+    @contact_detail_property.key = nil
+    assert !@contact_detail_property.save
+  end
 end

9cb6343f631fa848c0e01eb17a542d2dafe697fe
Hermann Loose <hermannloose@gmail.com>
2012-01-10 14:15:34 +0100

    Destroy contact detail properties with parent object.

--- app/models/contact_detail.rb
+++ app/models/contact_detail.rb
@@ -1,6 +1,6 @@
 class ContactDetail < ActiveRecord::Base
   belongs_to :user
-  has_many :contact_detail_properties
+  has_many :contact_detail_properties, :dependent => :destroy
 
   validates :name, :presence => true
   validates :user_id, :presence => true
--- test/unit/contact_detail_test.rb
+++ test/unit/contact_detail_test.rb
@@ -18,4 +18,14 @@ class ContactDetailTest < ActiveSupport::TestCase
     @contact_detail.user = nil
     assert !@contact_detail.save
   end
+
+  test "should destroy dependent contact_detail_properties" do
+    contact_detail_properties = @contact_detail.contact_detail_properties
+    @contact_detail.destroy
+    assert @contact_detail.destroyed?
+    contact_detail_properties.each do |property|
+      property.reload
+      assert property.destroyed?
+    end
+  end
 end

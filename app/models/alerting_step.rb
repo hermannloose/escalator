e0f6536e10e69505122b574af0d5e823fe8ca30b
Hermann Loose <hermannloose@gmail.com>
2012-01-11 20:54:15 +0100

    Validate delay in alerting steps & add ordered scope.

--- app/models/alerting_step.rb
+++ app/models/alerting_step.rb
@@ -2,6 +2,12 @@ class AlertingStep < ActiveRecord::Base
   belongs_to :contact_detail
   belongs_to :rotation_membership
 
+  validates :delay, :numericality => {
+    :only_integer => true,
+    :greater_than_or_equal_to => 0
+  }
   validates :contact_detail_id, :presence => true
   validates :rotation_membership_id, :presence => true
+
+  scope :ordered, order("delay")
 end
--- test/unit/alerting_step_test.rb
+++ test/unit/alerting_step_test.rb
@@ -9,6 +9,21 @@ class AlertingStepTest < ActiveSupport::TestCase
     assert @step.save
   end
 
+  test "should allow a delay of zero" do
+    @step.delay = 0
+    assert @step.save
+  end
+
+  test "should not allow negative delays" do
+    @step.delay = -1
+    assert !@step.save
+  end
+
+  test "should only allow integer delays" do
+    @step.delay = 1.1
+    assert !@step.save
+  end
+
   test "should validate presence of contact_detail" do
     @step.contact_detail = nil
     assert !@step.save

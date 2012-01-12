b16e555a06cf1f39b5b404a28cb19b67c4bd1b89
Hermann Loose <hermannloose@gmail.com>
2012-01-12 15:01:57 +0100

    Retrieve rotation members ordered by rank.

--- app/models/rotation.rb
+++ app/models/rotation.rb
@@ -1,6 +1,6 @@
 class Rotation < ActiveRecord::Base
   has_many :rotation_memberships
-  has_many :users, :through => :rotation_memberships
+  has_many :users, :through => :rotation_memberships, :order => "rank"
 
   validates :name, :presence => true
 end
--- app/models/rotation_membership.rb
+++ app/models/rotation_membership.rb
@@ -3,6 +3,12 @@ class RotationMembership < ActiveRecord::Base
   belongs_to :user
   has_many :alerting_steps
 
+  validates :rank, :numericality => {
+    :only_integer => true,
+    :greater_than_or_equal_to => 0
+  }
   validates :rotation_id, :presence => true
   validates :user_id, :presence => true
+
+  scope :ordered, order("rank")
 end

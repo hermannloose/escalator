cb90b77f745d954ed833160a841e2d5ba2d78ddb
Hermann Loose <hermannloose@gmail.com>
2012-01-12 17:22:27 +0100

    Add has_many association between assignees and issues.

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,5 +1,6 @@
 class Issue < ActiveRecord::Base
   belongs_to :escalation_policy
+  belongs_to :assignee, :class_name => "User"
 
   validates :escalation_policy_id, :presence => true
   validates :title, :presence => true

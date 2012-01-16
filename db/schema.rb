d235151258820c293136ad89b7abf02dbf806615
Hermann Loose <hermannloose@gmail.com>
2012-01-16 14:05:07 +0100

    Run migration for assignments.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120116125515) do
+ActiveRecord::Schema.define(:version => 20120116130254) do
 
   create_table "alerting_steps", :force => true do |t|
     t.integer  "delay_minutes"
@@ -22,6 +22,16 @@ ActiveRecord::Schema.define(:version => 20120116125515) do
 
   add_index "alerting_steps", ["contact_detail_id"], :name => "index_alerting_steps_on_contact_detail_id"
   add_index "alerting_steps", ["rotation_membership_id"], :name => "index_alerting_steps_on_rotation_membership_id"
+
+  create_table "assignments", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "role_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "assignments", ["role_id"], :name => "index_assignments_on_role_id"
+  add_index "assignments", ["user_id"], :name => "index_assignments_on_user_id"
 
   create_table "contact_detail_properties", :force => true do |t|
     t.integer  "contact_detail_id"

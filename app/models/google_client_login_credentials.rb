653789eb30c4e931789dd7221c108b4f52957f35
Hermann Loose <hermannloose@gmail.com>
2012-01-19 21:29:13 +0100

    Validate presence of email and token in ClientLogin credentials.

--- app/models/google_client_login_credentials.rb
+++ app/models/google_client_login_credentials.rb
@@ -1,2 +1,4 @@
 class GoogleClientLoginCredentials < ActiveRecord::Base
+  validates :email, :presence => true
+  validates :token, :presence => true
 end
--- test/unit/google_client_login_credentials_test.rb
+++ test/unit/google_client_login_credentials_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class GoogleClientLoginCredentialsTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @credentials = google_client_login_credentials(:valid)
+  end
+
+  test "should save valid credentials" do
+    assert @credentials.save
+  end
+
+  test "should validate presence of email" do
+    @credentials.email = nil
+    assert !@credentials.save
+  end
+
+  test "should validate presence of token" do
+    @credentials.token = nil
+    assert !@credentials.save
+  end
 end

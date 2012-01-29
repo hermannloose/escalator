6750d771590eb47dd7ec1fb6a9e6d931bc8152fd
Hermann Loose <hermannloose@gmail.com>
2012-01-29 18:53:23 +0100

    Customize config/deploy.rb and change production database.
    * use mysql2 adapter in production
    * deploy to escalator.burnandtremble.com for now
    * compile assets in production for now
    * provide deploy tasks for starting / stopping Thin

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,21 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
+require 'bundler/capistrano'
+require 'rvm/capistrano'
 
-set :scm, :subversion
+set :application, "escalator"
+set :repository,  "https://github.com/hermannloose/escalator.git"
+set :branch, "setup-capistrano"
+
+set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+set :deploy_to, "/home/hermann/deployments/escalator"
+set :use_sudo, false
+set :rvm_type, :user
+
+role :web, "escalator.burnandtremble.com"                          # Your HTTP server, Apache/etc
+role :app, "escalator.burnandtremble.com"                          # This may be the same as your `Web` server
+role :db,  "escalator.burnandtremble.com", :primary => true # This is where Rails migrations will run
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
@@ -20,3 +28,20 @@ role :db,  "your slave db-server here"
 #     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
 #   end
 # end
+
+namespace :deploy do
+  desc "Start Thin."
+  task :start do
+    run "cd #{deploy_to}/current && bundle exec thin start -C config/thin.yml"
+  end
+
+  desc "Stop Thin."
+  task :stop do
+    run "cd #{deploy_to}/current && bundle exec thin stop -C config/thin.yml"
+  end
+
+  desc "Restart Thin."
+  task :restart do
+    run "cd #{deploy_to}/current && bundle exec thin restart -C config/thin.yml"
+  end
+end

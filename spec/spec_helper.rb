8a32af8e621270b77b40e025e621513f906052f1
Hermann Loose <hermannloose@gmail.com>
2012-02-19 22:37:48 +0100

    Remove fixture path from spec_helper.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -29,9 +29,6 @@ RSpec.configure do |config|
   config.include Authorization::TestHelper, :type => :controller
   config.extend AuthorizationHelper, :type => :controller
 
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.

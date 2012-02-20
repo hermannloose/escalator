17020495aaddd3138c67cf2b638fe9666f514c66
Hermann Loose <hermannloose@gmail.com>
2012-02-20 14:28:53 +0100

    Add old spec_helper content to Spork#prefork.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,3 @@
-require 'rubygems'
 require 'spork'
 #uncomment the following line to use spork with the debugger
 #require 'spork/ext/ruby-debug'
@@ -7,7 +6,47 @@ Spork.prefork do
   # Loading more in this block will cause your tests to run faster. However,
   # if you change any configuration or code from libraries loaded here, you'll
   # need to restart spork for it take effect.
+  require 'simplecov'
+  SimpleCov.start "rails"
 
+  # This file is copied to spec/ when you run 'rails generate rspec:install'
+  ENV["RAILS_ENV"] ||= 'test'
+  require File.expand_path("../../config/environment", __FILE__)
+  require 'rspec/rails'
+  require 'rspec/autorun'
+  require 'shoulda-matchers'
+
+  require 'factory_girl_rails'
+  require 'declarative_authorization/maintenance'
+  require 'authorization_helper'
+
+  # Requires supporting ruby files with custom matchers and macros, etc,
+  # in spec/support/ and its subdirectories.
+  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+
+  RSpec.configure do |config|
+    # ## Mock Framework
+    #
+    # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
+    #
+    config.mock_with :mocha
+    # config.mock_with :flexmock
+    # config.mock_with :rr
+
+    config.include Devise::TestHelpers, :type => :controller
+    config.include Authorization::TestHelper, :type => :controller
+    config.extend AuthorizationHelper, :type => :controller
+
+    # If you're not using ActiveRecord, or you'd prefer not to run each of your
+    # examples within a transaction, remove the following line or assign false
+    # instead of true.
+    config.use_transactional_fixtures = true
+
+    # If true, the base class of anonymous controllers will be inferred
+    # automatically. This will be the default behavior in future versions of
+    # rspec-rails.
+    config.infer_base_class_for_anonymous_controllers = false
+  end
 end
 
 Spork.each_run do
@@ -43,48 +82,3 @@ end
 #
 # These instructions should self-destruct in 10 seconds.  If they don't, feel
 # free to delete them.
-
-
-
-
-require 'simplecov'
-SimpleCov.start "rails"
-
-# This file is copied to spec/ when you run 'rails generate rspec:install'
-ENV["RAILS_ENV"] ||= 'test'
-require File.expand_path("../../config/environment", __FILE__)
-require 'rspec/rails'
-require 'rspec/autorun'
-require 'shoulda-matchers'
-
-require 'factory_girl_rails'
-require 'declarative_authorization/maintenance'
-require 'authorization_helper'
-
-# Requires supporting ruby files with custom matchers and macros, etc,
-# in spec/support/ and its subdirectories.
-Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
-
-RSpec.configure do |config|
-  # ## Mock Framework
-  #
-  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-  #
-  config.mock_with :mocha
-  # config.mock_with :flexmock
-  # config.mock_with :rr
-
-  config.include Devise::TestHelpers, :type => :controller
-  config.include Authorization::TestHelper, :type => :controller
-  config.extend AuthorizationHelper, :type => :controller
-
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
-  config.use_transactional_fixtures = true
-
-  # If true, the base class of anonymous controllers will be inferred
-  # automatically. This will be the default behavior in future versions of
-  # rspec-rails.
-  config.infer_base_class_for_anonymous_controllers = false
-end
